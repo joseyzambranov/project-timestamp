05a6195c14aa689ee1e51c753adf109a19dc3017
jose zambrano <joseyzambranovpe@gmail.com>
2022-03-17 22:14:46 -0500

    finish timestamp

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -21,12 +22,52 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
+ 
   res.json({greeting: 'hello API'});
 });
+
+app.get("/api/",(req,res)=>{
+  let timeNow = new Date()
+  res.json({unix:timeNow.getTime(),utc:timeNow.toUTCString()})
+})
+
+app.get("/api/:date_string", function (req, res) {
+  let dateString = req.params.date_string
+  let passInValue = new Date(dateString)
+  let unix = passInValue.getTime()
+  let utc = passInValue.toUTCString()
+
+
+  if(parseInt(dateString)>10000){
+    dateInt = new Date(parseInt(dateString))
+
+    res.json({unix:dateInt.getTime(),utc:dateInt.toUTCString()})
+  }
+
+  if(passInValue == "Invalid Date"){
+    res.json({error : "Invalid Date"});
+  }else{
+    res.json({unix , utc});
+  }
+  
+
+  
+
+})
+/* code for me
+
+app.get("/api/:date_string", function (req, res) {
+  let utc = new Date(req.params.date_string)
+  let unix = Date.parse(req.params.date_string)
+
+  res.json({unix,utc})
+  
+  //res.json({error : "Invalid Date"});
+});*/
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
