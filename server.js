08d1750d18225efe4e5646ab3c6515e721be2677
jose zambrano <joseyzambranovpe@gmail.com>
2022-03-25 16:48:28 -0500

    delete mongodb

--- server.js
+++ server.js
@@ -69,29 +69,16 @@ app.get("/api/", (req, res) => {
 })
 
 //file medatada
-var FileMetadata = mongoose.model("FileMetadata", new mongoose.Schema({
-  name: String,
-  type:String,
-  size:String
 
-}))
 
 app.post("/api/fileanalyse",multer().single("upfile"),(req,res)=>{
   let reqfile = req.file
- let newFileMetadata = new FileMetadata({
-  name:reqfile.originalname,
-  type:reqfile.mimetype,
-  size:reqfile.size
- })
- newFileMetadata.save((err,doc)=>{
-   if(err) return console.error(err)
-   res.json({
+
+  res.json({
     name:reqfile.originalname,
     type:reqfile.mimetype,
     size:reqfile.size
   })
- })
-  
 })
 
 //function exercise-tracker
